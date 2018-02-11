5709a32e26e35606aeb80f171fb5b088481c89d6
kin29 <kin29.com@gmail.com>
2018-02-11 22:52:49 +0900

    コメント修正

--- admin-header-message/admin-header-message.php
+++ admin-header-message/admin-header-message.php
@@ -1,24 +1,33 @@
 <?php
 /**
- * Plugin Name: testPlugin
- * Description: Add testPlugin to your WordPress site.
- * Plugin URI: https://github.com/kin29/testPlugin
+ * Plugin Name: AdminMessage Maker
+ * Plugin URI: https://github.com/kin29/wp-plugin-test/tree/master/admin-header-message
+ * Description: You can set your like message in admin-page header.
+ * Version: 1.0.0
  * Author: kin29
  * Author URI: https://kin29.info
- * Version: 0.0.0
- * Text Domain: 
- * Domain Path: /languages/
- * License: GPLv2 or later
+ * License: GPLv2
  */
-//class mainClass
-//{
-  function addTest($x){
-  //$x='admin_bar_menu'
-   $x->add_menu( array(
-      'id' => 'my-menu',
-      'title' => 'hello, kin29!'
-    ) );
-  }
-//}
-//$mainObj = new mainClass();
+/*  Copyright 2018 ki29 (email : kin29.com@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+function addTest($x){
+//$x='admin_bar_menu'
+ $x->add_menu( array(
+    'id' => 'my-menu',
+    'title' => 'hello, kin29!'
+  ) );
+}
 add_action('admin_bar_menu', 'addTest');
